9931ea1eb9399fd61ab5dc11da92ae87edb8dcd9
Programming-Seungwan <swsj4480@gmail.com>
2023-12-06 01:36:31 +0900

    🎨 footer : replace text to icons by font awesome

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom';
 import styles from './footer.module.css';
 import { useDataContext } from '../../src/dataContext';
+import { faHouse, faMagnifyingGlass, faList12, faDiceSix, faWineBottle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Footer() {
   const { mode, handleAppMode } = useDataContext();
 
   function handleClickLink(ev) {
-    const pathname = ev.target.pathname;
-    console.log(pathname);
+    // nested 되기 때문에 pathname 속성을 존재하는 Link 컴포넌트를 포인팅하기 위해
+    const pathname = ev.currentTarget.pathname;
     if (pathname === '/category') handleAppMode('category');
     else if (pathname === '/random') handleAppMode('random');
     else if (pathname === '/ingredient') handleAppMode('ingredient');
@@ -20,28 +22,28 @@ export default function Footer() {
       <div className={styles.footerNavBarLinkContainer}>
         <button>
           <Link to='/home' onClick={handleClickLink}>
-            To home
+            <FontAwesomeIcon icon={faHouse} size='2x' />
           </Link>
         </button>
 
         <button>
           <Link to='/random' onClick={handleClickLink}>
-            By Random
+            <FontAwesomeIcon icon={faDiceSix} size='2x' />
           </Link>
         </button>
         <button>
           <Link to='/searchName' onClick={handleClickLink}>
-            By name
+            <FontAwesomeIcon icon={faMagnifyingGlass} size='2x' />
           </Link>
         </button>
         <button>
           <Link to='/category' onClick={handleClickLink}>
-            By Category
+            <FontAwesomeIcon icon={faList12} size='2x' />
           </Link>
         </button>
         <button>
           <Link to='/ingredient' onClick={handleClickLink}>
-            By Ingredient
+            <FontAwesomeIcon icon={faWineBottle} size='2x' />
           </Link>
         </button>
       </div>
