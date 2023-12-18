7643183b4acdf2cc1148a6655ab842966614da62
Programming-Seungwan <swsj4480@gmail.com>
2023-12-19 00:23:05 +0900

    Refactor : 개발자 프로필 페이지 수정

--- src/components/DeveloperDetail/DeveloperDetail.jsx
+++ src/components/DeveloperDetail/DeveloperDetail.jsx
@@ -6,7 +6,7 @@ const StyledDeveloperContainer = styled.div`
   flex: 1;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
   overflow-y: auto;
 `;
@@ -15,44 +15,66 @@ const StyledDeveloperImage = styled.img`
   border-radius: 15px;
   width: 250px;
   height: 250px;
-  margin: 40px;
+  margin: 50px;
 `;
 
 const StyledDeveloperNameP = styled.p`
-  font-family: GmarketSansMedium;
+  font-family: Helvetica;
   font-size: 25px;
+  width: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledDeveloperHisCareer = styled.div`
+  width: 90%;
 `;
 
 const StyledDeveloperCareerContainer = styled.div`
-  width: 300px;
+  width: 350px;
   height: auto;
   display: flex;
   flex-direction: column;
+  align-items: center;
 `;
 
 const StyledDeveloperCareerItem = styled.div`
-  width: 90%;
+  width: 100%;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  font-family: Helvetica;
+  margin-bottom: 20px;
 `;
 
 export default function DeveloperDetail() {
   return (
-    <StyledDeveloperContainer>
+    <StyledDeveloperContainer className='scroll-box'>
       <StyledDeveloperImage src='/assets/DeveloperProfile/seungwan.JPG' alt='developer photo image' />
-      <StyledDeveloperNameP>Kim Seung Wan</StyledDeveloperNameP>
+
+      <StyledDeveloperNameP>Seung Wan, Kim</StyledDeveloperNameP>
+
       <StyledDeveloperCareerContainer>
+        <StyledDeveloperNameP>Who is him?</StyledDeveloperNameP>
         <StyledDeveloperCareerItem>
           Whimoon 109th {'(from 2014 ~ 2016)'}
           <img src='/assets/DeveloperProfile/whimoon.webp' alt='whimoon logo' width='50px' height='50px' />
         </StyledDeveloperCareerItem>
+
         <StyledDeveloperCareerItem>
-          Hongik Business & Computer Engineering
+          Hongik Univ Biz Admin & CE
           <img src='/assets/DeveloperProfile/hongik.jpg' alt='hongik logo' width='50px' height='50px' />
         </StyledDeveloperCareerItem>
+
         <StyledDeveloperCareerItem>
-          1st Infantry Division Artillery Regiement
+          1st Division Artillery Regiment
           <img src='/assets/DeveloperProfile/junjin.JPG' alt='Military logo' width='50px' height='50px' />
+        </StyledDeveloperCareerItem>
+
+        <StyledDeveloperCareerItem>
+          Doosan Bears fan & LG Twins Hater
+          <img src='/assets/DeveloperProfile/doosan.png' alt='doosan logo' width='50px' height='50px' />
         </StyledDeveloperCareerItem>
       </StyledDeveloperCareerContainer>
     </StyledDeveloperContainer>
