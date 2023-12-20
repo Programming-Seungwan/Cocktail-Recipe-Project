4cc9d14e2788e6dd1052da6e2cf501f7fd0b18df
Programming-Seungwan <swsj4480@gmail.com>
2023-12-21 02:10:34 +0900

    Styles: searchName component input make prettier

--- src/components/SearchName/SearchName.jsx
+++ src/components/SearchName/SearchName.jsx
@@ -1,14 +1,9 @@
-// 이 파일에서는 칵테일의 이름을 통한 검색을 하고, 검색한 내용을 칵테일 카드로 보여준다
-// 처음에는 입력 form 만을 보여주고, 사용자가 검색을 하면 그때 관련된 정보들을 나타내주기
-// 이는 상태를 이용한다. 상태가 없어지면 그때에는 다시 Form만 보이게 만들어주기
-// form 과 cocktail Card가 왔다갔다 하는 방식
-// 다시 검색하기 버튼을 누르면 기존의 카드 사라지고, form이 나타나고
-// 이게 아니면 그냥 form만 존재하는 경우
-// 검색을 했는데 없다면 그냥 검색 결과가 존재하지 않음을 알려주어야 한다
 import { useRef, useState } from 'react';
 import styled from 'styled-components';
 import CocktailCard from '../CocktailCard/CocktailCard';
 import { getCocktailsByName } from '../../apiFuncs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const StyledSearchNameContainer = styled.div`
   margin: 7vh 0 10vh 0;
@@ -19,17 +14,43 @@ const StyledSearchNameContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   overflow-y: auto;
-  position: relative;
 `;
 
 // 최외곽 컨테이너에 바로 들어갈
 const StyledSearchFormContainer = styled.form`
-  width: 90%;
-  height: auto;
+  width: 85%;
+  height: 150px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
+  margin-top: 20px;
+`;
+
+const StyledSearchInputContainerDiv = styled.div`
+  width: 95%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--grey5);
+  padding: 0 15px;
+  border-radius: 15px;
+`;
+
+const StyledSearchFormInput = styled.input`
+  width: 70%;
+  line-height: 40px;
+  top: 10px;
+  border: none;
+  outline: none;
+  background-color: var(--grey5);
+  flex-grow: 0.8;
+  font-size: 16px;
+
+  &::placeholder {
+    font-size: 16px;
+  }
 `;
 
 const StyledFormRegenerateButton = styled.button`
@@ -41,7 +62,7 @@ const StyledFormRegenerateButton = styled.button`
 
 // 검색한 칵테일 카드들이 버튼과 겹치기에 position을 absolute로 해주고 부모인 껍데기 container를 relative로 만들어줌
 const StyledSearchCocktailsUL = styled.ul`
-  margin: 0;
+  margin: 45px 0 0 0;
   padding: 0;
   position: absolute;
   top: 90px;
@@ -54,10 +75,7 @@ export default function SearchName() {
 
   function handleFormSubmit(ev) {
     ev.preventDefault();
-    console.log(cocktailNameRef.current.value);
-    // 입력 폼은 제출되었고 이제 폼은 사라지고 데이터 칵테일카드가 보여지거나 데이터가 없으면 존재하지 않는다는 문구 표시
     setIsFormSubmit(true);
-
     getCocktailsByName(cocktailNameRef.current.value).then((data) => setSearchCocktailsData(data.drinks));
   }
 
@@ -66,24 +84,30 @@ export default function SearchName() {
     setSearchCocktailsData([]);
   }
 
+  function handleClickXIcon() {
+    cocktailNameRef.current.value = '';
+  }
+
   return (
     <StyledSearchNameContainer className='scroll-box'>
-      {/* 아직 제출 안됐을 때에는 form이 보이게 되고, 제출되면 실패 ui 카드나 칵테일 정보 카드가 위치 */}
+      {!isFormSubmit && (
+        <StyledSearchFormContainer onSubmit={handleFormSubmit}>
+          <StyledSearchInputContainerDiv>
+            <FontAwesomeIcon icon={faSearch} />
+            <StyledSearchFormInput type='text' placeholder='Cocktail Name' ref={cocktailNameRef} />
+            <FontAwesomeIcon icon={faXmark} className='hover' onClick={handleClickXIcon} />
+          </StyledSearchInputContainerDiv>
+        </StyledSearchFormContainer>
+      )}
+
       {isFormSubmit && (
         <StyledFormRegenerateButton onClick={handleClickRegenerateFormbutton}>
           Search Cocktail by name again!
         </StyledFormRegenerateButton>
       )}
-      {!isFormSubmit && (
-        <StyledSearchFormContainer onSubmit={handleFormSubmit}>
-          <p>Search by Cocktail name</p>
-          <input type='text' placeholder='Cocktail Name' ref={cocktailNameRef} />
-          <button type='submit'>Search</button>
-        </StyledSearchFormContainer>
-      )}
 
       {isFormSubmit && (
-        <StyledSearchCocktailsUL style={{ margin: '0', padding: '0' }}>
+        <StyledSearchCocktailsUL>
           {searchCocktailsData === null ? (
             <div>There is no search cocktail data!</div>
           ) : (
